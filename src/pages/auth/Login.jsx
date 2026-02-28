9fb0f5aecbc1937b03b0fa4944aa3b90b74e9951
rmustapha <134163158+Rohaenat-Eniola-Mustapha@users.noreply.github.com>
2026-02-28 11:42:00 +0100

    Add GraduationCap and Smartphone icons to Login component

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Mail, Lock, Eye, EyeOff } from 'lucide-react';
+import { Mail, Lock, Eye, EyeOff, GraduationCap, Smartphone } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../component/Button';
 import SuccessModal from '../../component/Modal';
